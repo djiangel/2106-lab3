8fb8b9ffc4c9153cfa5bd84d9935fb6116171411
djiangel <39209962+djiangel@users.noreply.github.com>
2021-10-20 01:04:31 +0800

    Add files via upload

--- ex3/packer.c
+++ ex3/packer.c
@@ -63,8 +63,8 @@ void pack_ball(int colour, int id, int *other_ids) {
     ball->id = id;
     ball->colour = colour;
     count_list[colour] -= 1;
-    printf("ballid: %d", id);
-    printf("%d: count: %d\n", count_list[colour]);
+    printf("ballid: %d\n", id);
+    printf("%d: count: %d\n", colour, count_list[colour]);
     if (head == NULL) {
         head = ball;
     } else {
@@ -92,25 +92,26 @@ void pack_ball(int colour, int id, int *other_ids) {
             prev = b;
             b = b->next;
         }
-        printf("other ids", other_ids);
+        printf("other ids\n");
+        printf(other_ids);
         for (int i = 1; i < number_per_pack; i++) {
             printf("signal 3x\n");
             sem_post(sem_list[colour]);
         }
         count_list[colour] += 1;
-        printf("%d should be 1", count_list[colour]);
+        printf("%d should be 1\n", count_list[colour]);
     }
     if (wait == false) {
-        printf("ballid: %d waiting here", id);
+        printf("ballid: %d waiting here\n", id);
         sem_wait(&mutex1);
     }
     sem_post(&mutex);
     if (wait) {
-        printf("ballid: %d waiting at %d", id, colour);
+        printf("ballid: %d waiting at %d\n", id, colour);
         sem_wait(sem_list[colour]);
     }
     if (count_list[colour] < number_per_pack) {
-        printf("ballid: %d in here", id);
+        printf("ballid: %d in here\n", id);
         b = head;
         int count = 0;
         while (b != NULL) {
@@ -124,7 +125,7 @@ void pack_ball(int colour, int id, int *other_ids) {
             b = b->next;
         }
         if (count_list[colour] == number_per_pack - 1) {
-            printf("ballid: %d in here once", id);
+            printf("ballid: %d in here once\n", id);
             b = head;
             while (b != NULL) {
                 if (b->colour == colour) {
@@ -140,7 +141,8 @@ void pack_ball(int colour, int id, int *other_ids) {
             sem_post(&mutex1);
         }
         count_list[colour] += 1;
-        printf("other ids2", other_ids);
-        printf("ending count: %d", count_list[colour]);
+        printf("other ids2\n");
+        printf(other_ids);
+        printf("ending count: %d\n", count_list[colour]);
     }
 }
